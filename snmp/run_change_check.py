8e793cc299166bdcda47f90e3f46793548f2d6b2
Carl Tewksbury <packetbuddha@gmail.com>
2015-05-07 20:20:48 -0700

    synch things up

--- snmp/run_change_check.py
+++ snmp/run_change_check.py
@@ -6,41 +6,27 @@ the router hostname and timestamp of the change.
 """
 
 from pynet_dev import devices, get_host_details
-from snmp_helper import snmp_get_oid_v3
+from snmp_helper import snmp_get_oid_v3, snmp_extract
 import pprint
-            
+
 
 routers_l = get_host_details(devices, model='881', snmp_port=8061)
-pprint.pprint(routers_l)
+#pprint.pprint(routers_l)
+
+oid_run_last_change = '1.3.6.1.4.1.9.9.43.1.1.1.0'
+oid_run_last_save = '1.3.6.1.4.1.9.9.43.1.1.2.0'
+oid_start_last_save = '1.3.6.1.4.1.9.9.43.1.1.3.0'
+
+for router in routers_l:
+	print router.keys()
+	print router.values()['ip_address']
+
+#	host = router['pynet-rtr1']['ip_address']
+#	snmp_port = devices['pynet-rtr1']['snmp_port']
+#	snmp_comm = devices['pynet-rtr1']['snmp_community']
 
 
 
 
-    
 
 
-
-"""
-def get_host_details(devices, **kvargs):
-    
-    Params:
-        devices : dict : device details
-        kvargs : keyword arguements : any number of host matching parameters
-    Return:
-        List of dict entries matching given match params
-    
-
-    if not len(kvargs):
-        return
-    
-    else:
-        pprint.pprint(kvargs)
-        results = []
-        for key, value in devices.items():
-            for val_key, val_val in value.items():
-                for arg_key, arg_val in kvargs.items():
-                    if arg_key in value:
-                        if arg_key == val_key and arg_val == val_val:
-                            results.append({key : value})
-        return results
-"""
